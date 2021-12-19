299944eada25f12a298a34807f42275df43e350f
catlair <catlair@qq.com>
2021-12-19 22:07:34 +0800

    feat(locale): 配置国际化

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
-  <router-view />
+  <a-config-provider :locale="zh_CN">
+    <router-view />
+  </a-config-provider>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 默认是英文，配置为中文
+import zh_CN from 'ant-design-vue/es/locale/zh_CN'
+</script>
 
 <style>
 #app {
